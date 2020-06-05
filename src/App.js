285c43a353bf2166dae1082590f8e36287e72a5f
Mila <kaah.fernandess@hotmail.com>
2020-06-04 22:17:04 -0300

    :construction: Aula 047 - Desafio Unico

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './sobre.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite o <code>src/App.js</code> e salve para reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
-      </header>
-    </div>
+    <h1 className="sobre">sobre</h1>
   );
 }
 
