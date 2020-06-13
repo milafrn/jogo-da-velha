2496002f9b712970ffa9d20451c56f9f8d705765
Kaah fernandes <kaah.fernandess@hotmail.com>
2020-06-12 22:28:34 -0300

    🚧 Criou o component Header sem o CSS

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import Logo from "../../objects/Logo";
+import Sobre from "../../objects/Sobre";
+import Menu from "../../objects/Menu";
 
-const Header = (props) => {
-return (<header className="header">{props.children}</header>)
-}
+const Header = () => (
+  <header className="header">
+    <Logo />
+    <Sobre />
+    <Menu />
+  </header>
+);
 
-export default Header
+export default Header;
