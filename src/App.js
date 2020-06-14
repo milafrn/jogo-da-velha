e1c2a6138c736c9dc7fe3f7981aa35ee3a6489d9
Kaah fernandes <kaah.fernandess@hotmail.com>
2020-06-14 19:12:58 -0300

    🏁 Aula 050 - Desafio Basico

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Board from "./components/board";
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Board />
+    </>
   );
 }
 
---
+++ src/components/board/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const Board = () => (
+  <div className="board"></div>
+)
+
+export default Board
