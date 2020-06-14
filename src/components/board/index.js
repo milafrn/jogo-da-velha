e73e91dfd02fde86e8c0ad2e7aa61127f88b6ea4
Kaah fernandes <kaah.fernandess@hotmail.com>
2020-06-14 19:51:33 -0300

    🏁 Aula 050 - Desafio Intermediario

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './styles.css';
+import Piece from '../../objects/Piece';
 
 const Board = () => (
-  <div className="board"></div>
+  <div className="board">
+    <Piece />
+  </div>
 )
 
 export default Board
