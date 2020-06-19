370f3bcb2df206b9b5ff50e426ecfbcb179c93fd
Kaah fernandes <kaah.fernandess@hotmail.com>
2020-06-19 20:36:44 -0300

    🚧 Criou o object Input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 
+import Input from "./objects/Input";
 import Label from "./objects/Label";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
@@ -9,6 +10,7 @@ const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
+    <Input value="show" />
     <Label content="Mostrar eventos" />
   </main>
 );
