f8a93fd6f4ac2709149ed6de8a8de078ebb58dc9
Kaah fernandes <kaah.fernandess@hotmail.com>
2020-06-23 19:08:52 -0300

    🏁 Aula 052 - Desafio basico

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 
 import Checkbox from "./objects/Checkbox";
+import Layer from "./components/Layer";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 
 const App = () => (
   <main className="app">
+    <Layer />
     <Header />
     <Hashtag />
     <Checkbox id="show" value="show" type="checkbox" content="Mostrar eventos" />
---
+++ src/components/Layer/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const Layer = () => (
+  <section className="layer"></section>
+);
+
+export default Layer;
