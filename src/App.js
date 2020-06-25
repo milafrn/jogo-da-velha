c6171c98f8203735fadcacfe1ea4f2b6b1dd740b
milafrn <kaah.fernandess@hotmail.com>
2020-06-25 13:38:30 -0300

    🚧 Adicionamos o AboutLink dentro do About, agora o About sabe lidar com o children

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Checkbox from "./objects/Checkbox";
 import About from "./objects/About";
+import AboutLink from "./objects/AboutLink";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 
@@ -17,7 +18,9 @@ const App = () => {
         type="checkbox"
         content="Mostrar eventos"
       />
-      <About />
+      <About>
+        <AboutLink />
+      </About>
     </main>
   );
 };
