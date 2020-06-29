55c5cd8d5d7cc88fe9af786e601d2ec85d388a7d
milafrn <kaah.fernandess@hotmail.com>
2020-06-29 19:05:02 -0300

    🚧 Criamos o CSS do ProfileUser

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Profile from '../../img/profile.jpeg';
 import AvatarProfile from "../../objects/AvatarProfile";
+import './styles.css';
+
 
 const ProfileUser = () => (
-  <dl>
-    <dd>
+  <dl className="profile-user">
+    <dd className="avatar">
       <AvatarProfile src={Profile} alt="Avatar do Marco Bruno" />
     </dd>
-    <dt>Mila</dt>
-    <dd>Sou estudante de Front-End. No momento não trabalho na área de programação, mas um dia ainda irei conseguir entrar no mercado de programação.</dd>
+    <dt className="title">Camila Fernandes</dt>
+    <dd className="description">Sou estudante de Front-End. No momento não trabalho na área de programação, mas um dia ainda irei conseguir entrar no mercado de programação.</dd>
   </dl>
 );
 
