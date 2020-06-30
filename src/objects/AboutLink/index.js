eab4220d452925c454e46d340bcb04998e18158b
milafrn <kaah.fernandess@hotmail.com>
2020-06-30 18:12:59 -0300

    🚧 Adicionamos o evento de click no AboutLink

--- src/objects/AboutLink/index.js
+++ src/objects/AboutLink/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./styles.css";
 
+const handleClick = () => {
+  console.log('click');
+}
+
 const AboutLink = ({ className = '' }) => (
-  <a href="#to-do" className={`about-link ${className}`}>
+  <a href="#to-do" className={`about-link ${className}`} onClick={handleClick}>
     Sobre
   </a>
 );
