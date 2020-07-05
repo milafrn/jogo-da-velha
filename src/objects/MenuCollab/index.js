c586da2061911e85c7372de732eda38ca74cc2fe
mila <kaah.fernandess@hotmail.com>
2020-07-05 11:19:25 -0300

    Aula 053 - Refactor name Component Menu to MenuCollab

--- src/objects/MenuCollab/index.js
+++ src/objects/MenuCollab/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./styles.css";
 
-const Menu = ({ onClick }) => (
+const MenuCollab = ({ onClick }) => (
   <a href="#menu" className="menu" onClick={onClick}>
     <span className="center">Menu</span>
   </a>
 );
 
-export default Menu;
+export default MenuCollab;
