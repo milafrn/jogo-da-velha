5c61094e7097cae66fb47398734e51c9aa2698a0
mila <kaah.fernandess@hotmail.com>
2020-07-13 17:55:33 -0300

    🚧 Refactoring do Menu para MenuGame

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import AboutLink from "../../objects/AboutLink";
 import LogoCollab from "../../objects/LogoCollab";
-import MenuCollab from "../../objects/MenuCollab";
+import MenuGame from "../../objects/MenuGame";
 import "./styles.css";
 
 const Header = ({ onClick }) => (
   <header className="header">
     <LogoCollab />
     <AboutLink onClick={onClick} />
-    <MenuCollab onClick={onClick} />
+    <MenuGame onClick={onClick} />
   </header>
 );
 
