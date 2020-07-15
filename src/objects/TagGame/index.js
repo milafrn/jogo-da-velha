3ae28b4c4f6d4418a1782bec5512c306bbc1952c
mila <kaah.fernandess@hotmail.com>
2020-07-15 20:56:23 -0300

    🚧 Criamos o CSS do TagGame

--- src/objects/TagGame/index.js
+++ src/objects/TagGame/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './styles.css';
 
 
 const TagGame = ({content}) => (
-  <output className="tag-name">{content}</output>
+  <output className="tag-game">{content}</output>
 );
 
 export default TagGame;
