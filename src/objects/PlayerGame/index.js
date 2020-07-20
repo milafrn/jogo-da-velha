d00e1d86a6185e7f7da5a12f25df2a65e066cf25
mila <kaah.fernandess@hotmail.com>
2020-07-20 20:15:27 -0300

    🚧 Preencher o PlayerGame com o player uma vez que ele foi clicado

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -1,20 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import playerO from "../../img/player-o.png";
 import playerX from "../../img/player-x.png";
 import "./styles.css";
 
-const handleClick = () => {
-  console.log('vamo que vamo!!')
-}
-
 const PlayerGame = ({ player = false }) => {
+  const [statePlayer, setStatePlayer] = useState(player)
   const players = [];
   players["x"] = playerX;
   players["o"] = playerO;
 
+  const handleClick = () => setStatePlayer('x');
+
   return (
     <button onClick={handleClick} className="player-game">
-      {player && <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`} />}
+      {statePlayer && <img src={players[statePlayer]} alt={`Jogador ${statePlayer.toUpperCase()}`} />}
     </button>
   );
 };
