7403ec87f591716195ad5327e3d10c111c20dde7
mila <kaah.fernandess@hotmail.com>
2020-07-21 20:59:15 -0300

    🏁 Aula 054 - Desafio basico

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -3,14 +3,16 @@ import playerO from "../../img/player-o.png";
 import playerX from "../../img/player-x.png";
 import "./styles.css";
 
-const PlayerGame = ({ player = false }) => {
+const PlayerGame = ({player = ''}) => {
   const [statePlayer, setStatePlayer] = useState(player)
   const players = [];
   players["x"] = playerX;
   players["o"] = playerO;
 
-  const handleClick = () => setStatePlayer('x');
-
+  const handleClick = () => {
+    setStatePlayer(statePlayer === 'x' ? 'o' : 'x');
+  }
+  
   return (
     <button onClick={handleClick} className="player-game">
       {statePlayer && <img src={players[statePlayer]} alt={`Jogador ${statePlayer.toUpperCase()}`} />}
