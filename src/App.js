f7a33ffd90c546cbf726ce3f9784013016a6254d
mila <kaah.fernandess@hotmail.com>
2020-07-23 21:01:01 -0300

    🚧 Mostra o history na tela, colocando ele no useState

--- src/App.js
+++ src/App.js
@@ -10,16 +10,12 @@ import LayerDark from "./objects/LayerDark";
 
 const App = () => {
   const [activeAbout, setActiveAbout] = useState("");
-  const history = [];
+  const [history, setHistory] = useState([]);
 
   const handleClickAdd = () => setActiveAbout("-active");
   const handleClickRemove = () => setActiveAbout('');
 
-  const addHistory = (player) => {
-    console.log('antes do push', history)
-    history.push(`Adicionou ${player.toUpperCase()}`);
-    console.log('depois do push', history)
-  }
+  const addHistory = (player) => setHistory(old => [...old, `Adicionou ${player}`])
 
   return (
     <main id="main" className="app">
