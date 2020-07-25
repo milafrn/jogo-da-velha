24abf23b1a2bcc0822c55374ee9fb6b544e4a6c2
mila <kaah.fernandess@hotmail.com>
2020-07-24 21:03:22 -0300

    🚧 Responsivo do HistoryGame

--- src/App.js
+++ src/App.js
@@ -7,31 +7,35 @@ import HistoryGame from "./components/HistoryGame";
 import ProfileUser from "./components/ProfileUser";
 import InputCheckbox from "./objects/InputCheckbox";
 import LayerDark from "./objects/LayerDark";
+import WrapperHashtagHistory from "./objects/WrapperHashtagHistory";
 
 const App = () => {
   const [activeAbout, setActiveAbout] = useState("");
   const [history, setHistory] = useState([]);
 
   const handleClickAdd = () => setActiveAbout("-active");
-  const handleClickRemove = () => setActiveAbout('');
+  const handleClickRemove = () => setActiveAbout("");
 
-  const addHistory = (player) => setHistory(old => [...old, `Adicionou ${player}`])
+  const addHistory = (player) =>
+    setHistory((old) => [...old, `Adicionou ${player}`]);
 
   return (
     <main id="main" className="app">
       <HeaderGame onClick={handleClickAdd} />
-      <HashtagGame callback={addHistory} />
-      <InputCheckbox
-        id="show"
-        value="show"
-        type="checkbox"
-        content="Mostrar eventos"
-      />
+      <WrapperHashtagHistory>
+        <HashtagGame callback={addHistory} />
+        <InputCheckbox
+          id="show"
+          value="show"
+          type="checkbox"
+          content="Mostrar eventos"
+        />
 
-      <HistoryGame history={history}/>
+        <HistoryGame history={history} />
+      </WrapperHashtagHistory>
 
       <LayerDark className={activeAbout}>
-        <HeaderInternal onClick={handleClickRemove}/>
+        <HeaderInternal onClick={handleClickRemove} />
         <ProfileUser />
       </LayerDark>
     </main>
